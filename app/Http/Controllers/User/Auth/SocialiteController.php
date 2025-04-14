1274e71709f859b9efd9527995f7039453d9a259
mrhero05 <ogiesanchez.hs@gmail.com>
2025-04-14 13:59:20 +0800

    Test login user

--- app/Http/Controllers/User/Auth/SocialiteController.php
+++ app/Http/Controllers/User/Auth/SocialiteController.php
@@ -4,9 +4,19 @@ namespace App\Http\Controllers\User\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Repositories\UserLoginRepository;
 
 class SocialiteController extends Controller
 {
     //
+    protected $UserLoginRepository;
+    public function __construct(UserLoginRepository $user_login_repository) {
+        $this->UserLoginRepository = $user_login_repository;
+    }
 
+    public function createOrUpdateUser($user)
+    {
+        dd($user->email);
+        $this->UserLoginRepository->existingUser($user->email);
+    }
 }
