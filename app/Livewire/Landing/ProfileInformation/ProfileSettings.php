adfe13f22abbbd564955e13f39f8d6b5b258444c
mrhero05 <ogiesanchez.hs@gmail.com>
2025-05-23 09:01:33 +0800

    update profile info layout

--- app/Livewire/Landing/ProfileInformation/ProfileSettings.php
+++ app/Livewire/Landing/ProfileInformation/ProfileSettings.php
@@ -7,9 +7,19 @@ use Livewire\Component;
 class ProfileSettings extends Component
 {
     public $selectedTab = 'profile';
+    public $sidebar = 'block';
+    public $panel = 'hidden md:flex';
     public function selectNav($tab)
     {
         $this->selectedTab = $tab;
+        $this->sidebar = 'hidden md:block';
+        $this->panel = 'flex';
+    }
+
+    public function backToMenu()
+    {
+        $this->sidebar = 'block';
+        $this->panel = 'hidden md:flex';
     }
 
     public function render()
